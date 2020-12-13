a74615781a3cc9ee4c05598566b9263b44482876
subs <1262945983@qq.com>
2020-12-13 23:16:37 +0800

    yijiao

--- gateway-server/src/main/java/com/zhitong/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/zhitong/gatewayserver/GatewayServerApplication.java
@@ -2,8 +2,9 @@ package com.zhitong.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
